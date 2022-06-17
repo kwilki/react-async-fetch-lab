6e9c4e02b1f59c9b835af14dc257545352041aad
kwilki <kylewilkinson14@gmail.com>
2022-06-17 18:51:14 +1000

    finished, app behaving, tests not working

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, {Component} from 'react'
+
+export default class App extends Component {
+
+    state = {
+        peopleInSpace: []
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Number of people in space: {this.state.number}</h1>
+                {this.state.peopleInSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(response => response.json())
+            .then(({people, number}) => this.setState({peopleInSpace: people, number: number}, () => console.log(this.state)))
+            // OR
+            //.then((json) => this.setState({peopleInSpace: json.people, number: json.number}, () => console.log(this.state)))
+    }
+}
